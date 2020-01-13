373a12b1b4f674a1829e97746854552078976867
hill2770 <59539869+hill2770@users.noreply.github.com>
2020-01-13 10:52:34 -0500

    Changed install scripts

--- install_script_with_plots.R
+++ install_script_with_plots.R
@@ -1,12 +1,15 @@
-to.install<-c('usethis', 'learnr', 'devtools', 'tidyverse', 'magrittr', 'lubridate', 'haven', 'rmarkdown', 'datapasta', 'tibble', 'readr', 'foreign', 'dplyr', 'labelled', 'questionr', 'readxl',  'openxlsx', 'knitr', 'jsonlite', 'tidyr', 'DT', 'stringr', 'ggthemes', 'ggbeeswarm', 'glue', 'stringi', 'ggridges', 'viridis', 'scales', 'sf', 'tidycensus', 'tigris', 'censusapi', 'ggmap', 'leaflet', 'httpuv', 'cancensus', 'statcanR', 'cansim', 'valetr', 'weathercan', 'CANSIM2R', 'rcanvec', 'opendatatoronto', 'VancouvR', 'guardianapi', 'GuardianR', 'gapminder', 'broom', 'coefplot', 'cowplot', 'GGally', 'ggrepel', 'gridExtra', 'here', 'interplot', 'margins', 'maps', 'mapproj', 'mapdata', 'MASS', 'quantreg', 'rlang', 'survey', 'srvyr', 'viridisLite')
+to.install<-c('usethis', 'RTools','learnr', 'devtools', 'tidyverse', 'magrittr', 'lubridate', 'haven', 'rmarkdown', 'datapasta', 'tibble', 'readr', 'foreign', 'dplyr', 'labelled', 'questionr', 'readxl',  'openxlsx', 'knitr', 'jsonlite', 'tidyr', 'DT', 'stringr', 'ggthemes', 'ggbeeswarm', 'glue', 'stringi', 'ggridges', 'viridis', 'scales', 'sf', 'tidycensus', 'tigris', 'censusapi', 'ggmap', 'leaflet', 'httpuv', 'cancensus', 'statcanR', 'cansim', 'valetr', 'weathercan', 'CANSIM2R', 'rcanvec', 'opendatatoronto', 'VancouvR', 'guardianapi', 'GuardianR', 'gapminder', 'broom', 'coefplot', 'cowplot', 'GGally', 'ggrepel', 'gridExtra', 'here', 'interplot', 'margins', 'maps', 'mapproj', 'mapdata', 'MASS', 'quantreg', 'rlang', 'survey', 'srvyr', 'viridisLite')
 
 install.lib<-to.install[!to.install %in% installed.packages()]
+
 for(lib in install.lib) install.packages(lib,dependencies=TRUE)
 
+
+
 #Install from github
-devtools::install_github("andrewbtran/muckrakr")
-devtools::install_github("AliciaSchep/gglabeller") 
-devtools::install_github("kjhealy/socviz") 
+devtools::install_github("andrewbtran/muckrakr", force=T)
+devtools::install_github("AliciaSchep/gglabeller", force=T) 
+devtools::install_github("kjhealy/socviz", force=T)
 
 library(ggplot2)
 var1<-seq(1,100,1)
@@ -20,4 +23,5 @@ var3<-sample(c('A', 'B', 'C'), 100, replace=T)
 var4<-Recode(var3, "'A'='Apple'; 'B'='Banana' ; 'C'='Cactus'")
 data.frame(var3, var4) %>% 
   ggplot(., aes(x=var4))+geom_bar()+labs(title='IF you get this plot too things are working. ')
-ggsve('test_plot_2.png')
+ggsave('test_plot_2.png')
+
