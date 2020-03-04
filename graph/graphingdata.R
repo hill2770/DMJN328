6f9bd70375175d4ad894d64a1994d63f9b43c785
hill2770 <59539869+hill2770@users.noreply.github.com>
2020-03-04 11:05:16 -0500

    JonnicaGraphRoughDraftPush

--- graph/graphingdata.R
+++ graph/graphingdata.R
@@ -1,8 +1,9 @@
+
 spotify_tidy
 
 library(ggplot2)
 
-ggplot(danceability,
-       aes (x="Track Name", y="dancability")) +
-  geom_bar()
-  
+ggplot(spotify_tidy, aes(x=Song, y=Danceability, fill=Speechiness)) + 
+  geom_col(position="dodge", stat="identity") + scale_fill_gradient(low="chartreuse", high="darkgreen")+
+  labs(title="Danceability and Speechiness of Spotify's 2019 Top 10", caption="Source: https://www.kaggle.com/prasertk/spotify-global-2019-moststreamed-tracks/metadata")
+
--- graph/tidydata.R
+++ graph/tidydata.R
@@ -7,6 +7,8 @@ spotify_tidy <- spotify_global %>%
   filter(Rank<11)
 
 
-spotify_tidy %>%
-  str_to_title(colnames)
+
+spotify_tidy <- select(spotify_tidy, "Track Name", danceability, speechiness)
+
+spotify_tidy <- rename(spotify_tidy, Danceability=danceability, Speechiness=speechiness, Song="Track Name")
 
