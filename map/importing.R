f27f201b9e7edc21df0518b41002ffb7cc09e5fc
hill2770 <59539869+hill2770@users.noreply.github.com>
2020-03-19 14:54:45 -0400

    LATEST UPDATE
    Changed to toatal just BA not BA or higher

--- map/importing.R
+++ map/importing.R
@@ -22,10 +22,10 @@ ggplot(hamilton, aes(geometry=geometry, fill=Population))+geom_sf()
 
 
 #Importing university BA vector
-hamilton2 <- get_census(dataset='CA16', regions=list(CD="3525"), vectors=c("v_CA16_5078"), labels="detailed", geo_format="sf", level='DA')
+hamilton2 <- get_census(dataset='CA16', regions=list(CD="3525"), vectors=c("v_CA16_5081"), labels="detailed", geo_format="sf", level='DA')
 names(hamilton2)
 
-hamilton2 <- rename(hamilton2, "Total"="v_CA16_5078: University certificate, diploma or degree at bachelor level or above")
+hamilton2 <- rename(hamilton2, "TotalBA"="v_CA16_5081: Bachelor's degree")
 names (hamilton2)
 
 ggplot(hamilton2, aes(geometry=geometry, fill=TotalBA))+geom_sf()
@@ -34,10 +34,10 @@ ggplot(hamilton2, aes(geometry=geometry, fill=TotalBA))+geom_sf()
 
 
 #Male and Female
-hamilton3 <- get_census(dataset='CA16', regions=list(CD="3525"), vectors=c("v_CA16_5082","v_CA16_5083"), labels="detailed", geo_format=NA, level='DA')
+hamilton3 <- get_census(dataset='CA16', regions=list(CD="3525"), vectors=c("v_CA16_5081", "v_CA16_5082","v_CA16_5083"), labels="detailed", geo_format=NA, level='DA')
 names(hamilton3)
 
-hamilton4 <- rename (hamilton3, "FemaleBA"= "v_CA16_5083: Bachelor's degree", "MaleBA"="v_CA16_5082: Bachelor's degree")
+hamilton4 <- rename (hamilton3, "TotalBA"="v_CA16_5081: Bachelor's degree", "FemaleBA"= "v_CA16_5083: Bachelor's degree", "MaleBA"="v_CA16_5082: Bachelor's degree")
 names(hamilton4)
 
-ggplot(hamilton4, aes(geometry=geometry, fill=n)) +geom_sf()
+ggplot(hamilton4, aes(geometry=geometry, fill=TotalBA)) +geom_sf()
