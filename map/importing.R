4f8c203a592e0f2b026247f580f4282bae2aee60
hill2770 <59539869+hill2770@users.noreply.github.com>
2020-03-19 14:30:29 -0400

    map update
    having problems plotting male and female code, says object geometry cannot be found.

--- map/importing.R
+++ map/importing.R
@@ -1,33 +1,43 @@
 #start my assignment here 
 
 install.packages('cancensus')
-
 library(cancensus)
 
 file.edit(file.path('~/.Rprofile'))
-
 options(cancensus.api_key='CensusMapper_059bce0f73d8e517369463bbb52c2412')
-
 options(cancensus.cache_path="~")
 
+#Getting the inital map
 hamilton <- get_census(dataset='CA16', regions=list(CD="3525"), vectors=c(), labels="detailed", geo_format="sf", level='DA')
 
 library(tidyverse)
-
+library(ggplot2)
 glimpse(hamilton)
 
-library(ggplot2)
-
+#Plotting empty map
 ggplot(hamilton, aes(geometry=geometry))+geom_sf()
 
+#Testing fill with population
 ggplot(hamilton, aes(geometry=geometry, fill=Population))+geom_sf()
 
+
+#Importing university BA vector
 hamilton2 <- get_census(dataset='CA16', regions=list(CD="3525"), vectors=c("v_CA16_5078"), labels="detailed", geo_format="sf", level='DA')
-
-glimpse(hamilton2)
-
 names(hamilton2)
 
-hamilton3 <- rename(hamilton2, University = "v_CA16_5078: University certificate, diploma or degree at bachelor level or above")
+hamilton2 <- rename(hamilton2, "TotalBA"="v_CA16_5078: University certificate, diploma or degree at bachelor level or above")
+names (hamilton2)
 
-ggplot(hamilton3, aes(geometry=geometry, fill=University))+geom_sf()
+ggplot(hamilton2, aes(geometry=geometry, fill=TotalBA))+geom_sf()
+
+
+
+
+#Male and Female
+hamilton3 <- get_census(dataset='CA16', regions=list(CD="3525"), vectors=c("v_CA16_5082","v_CA16_5083"), labels="detailed", geo_format=NA, level='DA')
+names(hamilton3)
+
+hamilton4 <- rename (hamilton3, "Female"= "v_CA16_5083: Bachelor's degree", "Male"="v_CA16_5082: Bachelor's degree")
+names(hamilton4)
+
+ggplot(hamilton4, aes(geometry=geometry, fill=n))+geom_sf()
